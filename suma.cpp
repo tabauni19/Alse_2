f6c68bc8ebfd1fd7696328af6f3a4b22d88a4f40
Dylan <sara.tabares-b@mail.escuelaing.edu.co>
2025-09-18 15:57:08 -0500

    suma

---
+++ suma.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "¿Cuántos números quieres ingresar? ";
+    cin >> n;
+
+    int* arr = new int[n];  // Crear arreglo dinámico según el tamaño ingresado
+    int suma = 0;
+
+    cout << "Ingresa " << n << " números:" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        suma += arr[i];
+    }
+
+    cout << "La suma de los elementos es: " << suma << endl;
+
+    delete[] arr;  // Liberar memoria dinámica
+    return 0;
+}
